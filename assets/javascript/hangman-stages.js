2dc0fa8d62a7a8b5cc7dee4ae2ff2fef1c5a87cb
Mark Kazanski <markkazanski@gmail.com>
2017-10-07 11:33:54 -0500

    added stuf

---
+++ assets/javascript/hangman-stages.js
@@ -0,0 +1,26 @@
+var hangmanStages = [ 
+
+    "_____<br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|<br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br />_|_<br />|&nbsp;&nbsp;&nbsp;&nbsp;|________<br />|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|<br />|__________|"
+    ,
+
+    "_____<br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|<br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;o <br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br />_|_<br />|&nbsp;&nbsp;&nbsp;&nbsp;|________<br />|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|<br />|__________|"
+    ,
+
+    "_____<br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|<br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;o <br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/<br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br />_|_<br />|&nbsp;&nbsp;&nbsp;&nbsp;|________<br />|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|<br />|__________|"
+    ,
+
+    "_____<br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|<br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;o <br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/|<br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br />_|_<br />|&nbsp;&nbsp;&nbsp;&nbsp;|________<br />|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|<br />|__________|"
+    ,
+
+    "_____<br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|<br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;o <br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/|\<br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br />_|_<br />|&nbsp;&nbsp;&nbsp;&nbsp;|________<br />|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|<br />|__________|"
+
+    ,
+
+    "_____<br />    &nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|<br />    &nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;o <br />    &nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/|\<br />    &nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|<br />    &nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/ <br />    _|_<br />    |&nbsp;&nbsp;&nbsp;&nbsp;|________<br />  |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|<br />    |__________|"
+    
+    ,
+
+    "_____<br /\>        &nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|<br /\>        &nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;o <br /\>        &nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/|\<br /\>        &nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|<br /\>        &nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/ \<br /\>        _|_<br /\>        |&nbsp;&nbsp;&nbsp;&nbsp;|________<br /\>        |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|<br /\>        |__________|"
+     
+
+];
---
+++ assets/javascript/hangman-stages2.js
@@ -0,0 +1,33 @@
+var hangmanStages = [
+
+"_____<br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|<br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;o <br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/|\\<br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|<br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/ \\<br />_|_<br />|&nbsp;&nbsp;&nbsp;&nbsp;|________<br />|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|<br />|__________|</p>"
+
+,
+
+"_____<br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|<br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;o <br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/|\\<br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|<br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/ <br />_|_<br />|&nbsp;&nbsp;&nbsp;&nbsp;|________<br />|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|<br />|__________|</p>"
+
+,
+
+"_____<br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|<br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;o <br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/|\\<br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|<br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br />_|_<br />|&nbsp;&nbsp;&nbsp;&nbsp;|________<br />|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|<br />|__________|</p>"
+
+,
+
+"_____<br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|<br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;o <br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/|\\<br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br />_|_<br />|&nbsp;&nbsp;&nbsp;&nbsp;|________<br />|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|<br />|__________|</p>"
+
+,
+
+"_____<br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|<br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;o <br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/|\<br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br />_|_<br />|&nbsp;&nbsp;&nbsp;&nbsp;|________<br />|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|<br />|__________|</p>"
+
+,
+
+"_____<br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|<br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;o <br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/<br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br />_|_<br />|&nbsp;&nbsp;&nbsp;&nbsp;|________<br />|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|<br />|__________|</p>"
+
+,
+
+"_____<br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|<br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;o <br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br />_|_<br />|&nbsp;&nbsp;&nbsp;&nbsp;|________<br />|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|<br />|__________|</p>"
+
+,
+
+"_____<br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|<br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br />&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br />_|_<br />|&nbsp;&nbsp;&nbsp;&nbsp;|________<br />|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|<br />|__________|</p>"
+
+];
